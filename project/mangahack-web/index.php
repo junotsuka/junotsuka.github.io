8e76c99f39d11308ec90f2604023bfb957c90dae
大塚 潤 <otsuka@echoes.co.jp>
2019-02-08 19:53:10 +0900

    fix 08

---
+++ project/mangahack-web/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<meta name="format-detection" content="telephone=no">
+<title>JUN OTSUKA</title>
+<link rel="stylesheet" href="/css/master.css">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/5.0.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+<link rel="shortcut icon" href="/images/favicon.ico">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+<script src="/js/raindrops.js"></script>
+<script>
+$(function(){
+$('.waveCanvas').raindrops(
+  {color:'#f6f6f6', rippleSpeed:0.1, canvasHeight:220}
+);});
+</script>
+</head>
+
+<body id="project">
+<div class="cross"><a href="/">X</a></div>
+<section>
+  <h1>MANGAHCK WEB</h1>
+  <dl>
+    <dt>2013〜</dt>
+    <dd>Planning / Design / Front-Coding</dd>
+  </dl>
+  <div class="article">
+    <div class="thumb"><img src="/images/project/mangahack-web/001.jpg"></div>
+    <h2>マンガハック WEB</h2>
+    <em class="url"><a href="https://mangahack.com/" target="_blank" rel="noopener noreferrer">https://mangahack.com/</a></em>
+    <p>LOGI Terminal様のWebサイトを制作させていただきました。物流専門職の養成や物流相談など、物流業務改善に関わる業務をされています。定期的に更新されるブログはとても面白いので、物流に興味のある方はぜひのぞいてみてください。プランニングからデザイン、コーディングまで担当させていただいています。</p>
+    <div class="thumb">
+      <img src="/images/project/mangahack-web/001.jpg">
+      <img src="/images/project/mangahack-web/001.jpg">
+      <img src="/images/project/mangahack-web/001.jpg">
+      <img src="/images/project/mangahack-web/001.jpg">
+    </div>
+  </div>
+</section>
+<footer>
+  <p>© JUN OTSUKA All rights reserved.</p>
+</footer>
+<div class="waveCanvas"></div>
+</body>
+</html>
